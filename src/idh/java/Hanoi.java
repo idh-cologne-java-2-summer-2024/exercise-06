9a5154f93b368d4b0bd482b7392331af6220ce38
limpl <limpl@192.168.45.229>
2024-06-02 21:10:44 +0200

    Fragen über Fragen.

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,15 +3,67 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
 
 	public Hanoi() {
+		
+	Stack<Integer> l = new Stack <>();
+	l.add(9);
+	l.add(8);
+	l.add(7);
+	l.add(6);
+	l.add(5);
+	l.add(4);
+	l.add(3);
+	l.add(2);
+	l.add(1);
+	Stack<Integer> m = new Stack <>();
+	Stack<Integer> r = new Stack <>();
 		// TODO: Implement
+	
+	
 	}
 	
-	private void movePiece(char from, char to) {
-		// TODO: Implement
+// Wenn ein Stab leer ist, kann nicht von ihm gezogen werden
+// Ich muss sicher stellen, dass die Nummer, die gezogen wird, entweder auf ein leeren Platz oder einen 
+// kleineren Platz passt
+// Ich muss drei Stacks erstellen, mit den jeweiligen Namen. 
+// es handelt sich hier um einen Stack, von daher erstmal erstellen, dass es drei Stück gibt. 
+	private void movePiece(char from, char to) { 
+		int ffrom = 0; 
+		int tto= 12; 
+		 
+		
+		switch (from) {
+        case 'l':
+             getLeftDescendingIterator();
+             
+        case 'm':
+        	 getMiddleDescendingIterator();
+        case 'r':
+        	 getRightDescendingIterator();
+        	}
+		
+		switch (to) {
+		case 'l':
+            getLeftDescendingIterator(); 
+		case 'm':
+       	 	getMiddleDescendingIterator();
+		case 'r':
+       	 	getRightDescendingIterator();
+       	}
+		
+		if (from > to) {
+			// move from to to 
+		
+		}
+		
+		
+//peek (from, to) wenn bei to nichts ist, dann from nach to oder wenn bei to kleiner ist, als bei 
+// from, dann from nach to 
+// pop (from), push (to)
 	}
 	
 	public void run() {
@@ -34,10 +86,12 @@ public class Hanoi {
 	}
 	
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
-
+		
+		return null; 
 	}
+	
+
+	
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		// TODO: Implement
 		return null;
@@ -76,6 +130,8 @@ public class Hanoi {
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
+		
+
 	}
 
 }
