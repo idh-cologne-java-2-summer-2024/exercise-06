80d0c4f3d1ecf299b8e9172d1362d5e59b479dcb
Hasan Atici <hasanatici@MacBook-Air-von-Hasan.local>
2024-06-03 00:04:19 +0300

    Exercise 06

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,62 +3,68 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
-	String documentText;
+    String documentText;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    public static Document readFromFile(File f) throws IOException {
+        FileReader fileReader = new FileReader(f);
+        int ch;
+        StringBuilder b = new StringBuilder();
+        while ((ch = fileReader.read()) != -1) {
+            b.append((char) ch);
+        }
+        fileReader.close();
+        Document doc = new Document();
+        doc.documentText = b.toString();
+        return doc;
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
-	}
+    public String getDocumentText() {
+        return documentText;
+    }
 
-	@Override
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
+    public void setDocumentText(String documentText) {
+        this.documentText = documentText;
+    }
 
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
+    public double ttr() {
+        Set<String> types = new HashSet<>();
+        int tokens = 0;
 
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
-	}
-	
-	
+        for (String token : this) {
+            types.add(token);
+            tokens++;
+        }
+
+        return (double) types.size() / tokens;
+    }
+
+    public static final void main(String[] args) throws IOException {
+        // Lese die Datei dracula.txt und berechne die TTR
+        Document d = Document.readFromFile(new File("/Users/hasanatici/git/exercise-06/data/dracula.txt"));
+        System.out.println("TTR: " + d.ttr());
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            StringTokenizer tokenizer = new StringTokenizer(documentText);
+
+            @Override
+            public boolean hasNext() {
+                return tokenizer.hasMoreTokens();
+            }
+
+            @Override
+            public String next() {
+                return tokenizer.nextToken();
+            }
+        };
+    }
 }
+
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,79 +3,114 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
 
-	public Hanoi() {
-		// TODO: Implement
-	}
-	
-	private void movePiece(char from, char to) {
-		// TODO: Implement
-	}
-	
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-				System.out.println(this);
-				System.out.print("Enter source and target stick (will move top piece):");
-				String s = br.readLine();
-				if (s.matches("^([lmr])([lmr])$")) {
-					char source = s.charAt(0);
-					char target = s.charAt(1);
-					movePiece(source, target);
-				}
-			} catch (Exception e) {
-				System.out.println("Try again, something's not right.");
-				// e.printStackTrace();
-			} 
-		}
-	}
-	
-	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+    private Stack<Integer> left;
+    private Stack<Integer> middle;
+    private Stack<Integer> right;
 
-	}
-	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+    public Hanoi() {
+        left = new Stack<>();
+        middle = new Stack<>();
+        right = new Stack<>();
+        // Initialisiere den linken Stab mit den Scheiben 9 bis 1
+        for (int i = 9; i >= 1; i--) {
+            left.push(i);
+        }
+    }
 
-	}
-	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
-	}
-	
-	public String toString() {
-		StringBuilder b = new StringBuilder();
-		b.append("  |\n l|");
-		Iterator<Integer> iter;
-		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n m|");
-		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n r|");
-		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |");
-		return b.toString();
-	}
-	
-	public static void main(String[] args) {
-		Hanoi hanoi = new Hanoi();
-		hanoi.run();
-	}
+    private void movePiece(char from, char to) {
+        Stack<Integer> source = getStack(from);
+        Stack<Integer> target = getStack(to);
 
+        if (source.isEmpty()) {
+            System.out.println("Source stack is empty. Invalid move.");
+            return;
+        }
+
+        int piece = source.peek();
+        if (!target.isEmpty() && target.peek() < piece) {
+            System.out.println("Invalid move. Cannot place larger piece on a smaller one.");
+            return;
+        }
+
+        source.pop();
+        target.push(piece);
+    }
+
+    private Stack<Integer> getStack(char c) {
+        switch (c) {
+            case 'l':
+                return left;
+            case 'm':
+                return middle;
+            case 'r':
+                return right;
+            default:
+                throw new IllegalArgumentException("Invalid stack identifier: " + c);
+        }
+    }
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.println(this);
+                System.out.print("Enter source and target stick (will move top piece):");
+                String s = br.readLine();
+                if (s.matches("^([lmr])([lmr])$")) {
+                    char source = s.charAt(0);
+                    char target = s.charAt(1);
+                    movePiece(source, target);
+                }
+            } catch (Exception e) {
+                System.out.println("Try again, something's not right.");
+                // e.printStackTrace();
+            }
+        }
+    }
+
+    private Iterator<Integer> getLeftDescendingIterator() {
+        return left.iterator();
+    }
+
+    private Iterator<Integer> getMiddleDescendingIterator() {
+        return middle.iterator();
+    }
+
+    private Iterator<Integer> getRightDescendingIterator() {
+        return right.iterator();
+    }
+
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append("  |\n l|");
+        Iterator<Integer> iter;
+        iter = this.getLeftDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |\n m|");
+        iter = this.getMiddleDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |\n r|");
+        iter = this.getRightDescendingIterator();
+        while (iter.hasNext()) {
+            b.append(iter.next());
+            b.append(' ');
+        }
+        b.append("\n  |");
+        return b.toString();
+    }
+
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi();
+        hanoi.run();
+    }
 }
