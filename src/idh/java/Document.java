5823f2a1d4e8b73d5ac032ce6ed696c56a46963d
Kirby <Kirby@LAPTOP-TBIH83MV.speedport.ip>
2024-06-04 20:40:28 +0200

    Übung 6

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -5,60 +5,71 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Document implements Iterable<String> {
-	String documentText;
+    String documentText;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    public static Document readFromFile(File f) throws IOException {
+        FileReader fileReader = new FileReader(f);
+        int ch;
+        StringBuilder b = new StringBuilder();
+        while( (ch = fileReader.read()) != -1 ) {
+            b.append((char) ch);
+        }
+        fileReader.close();
+        Document doc = new Document();
+        doc.documentText = b.toString();
+        
+        return doc;
+    }
+    
+    public String getDocumentText() {
+        return documentText;
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
-	}
+    public void setDocumentText(String documentText) {
+        this.documentText = documentText;
+    }
 
-	@Override
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
+    public double ttr() {
+        Set<String> uniqueWords = new HashSet<>();
+        int totalWords = 0;
 
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
+        for (String token : this) {
+            uniqueWords.add(token);
+            totalWords++;
+        }
 
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
-	}
-	
-	
+        if (totalWords == 0) {
+            return 0.0; // To avoid division by zero
+        }
+
+        return (double) uniqueWords.size() / totalWords;
+    }
+
+    public static final void main(String[] args) throws IOException {
+        Document d = Document.readFromFile(new File("data/dracula.txt"));
+        System.out.println("TTR: " + d.ttr());
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+
+            StringTokenizer tokenizer = new StringTokenizer(documentText);
+            
+            @Override
+            public boolean hasNext() {
+                return tokenizer.hasMoreTokens();
+            }
+
+            @Override
+            public String next() {
+                return tokenizer.nextToken();
+            }
+            
+        };
+    }
 }
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,79 +3,113 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.LinkedList;
+
+
 
 public class Hanoi {
 
-	public Hanoi() {
-		// TODO: Implement
-	}
-	
-	private void movePiece(char from, char to) {
-		// TODO: Implement
-	}
-	
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			try {
-				System.out.println(this);
-				System.out.print("Enter source and target stick (will move top piece):");
-				String s = br.readLine();
-				if (s.matches("^([lmr])([lmr])$")) {
-					char source = s.charAt(0);
-					char target = s.charAt(1);
-					movePiece(source, target);
-				}
-			} catch (Exception e) {
-				System.out.println("Try again, something's not right.");
-				// e.printStackTrace();
-			} 
-		}
-	}
-	
-	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+ private LinkedList<Integer> l;
+ private LinkedList<Integer> m;
+ private LinkedList<Integer> r;
+ public Hanoi() {
+  l = new LinkedList<Integer>();
+  m = new LinkedList<Integer>();
+  r = new LinkedList<Integer>();
+  for (int i = 1; i < 10; i++) {
+   l.add(i);
+   System.out.print(i + ", ");
+  }
+  System.out.println();
+  System.out.println();
+  
+ }
+ //lm
+ private void movePiece(char from, char to) {
 
-	}
-	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+  LinkedList<Integer> fromList = getListFromChar(from);
+  LinkedList<Integer> toList = getListFromChar(to);
+  
+  if(!toList.isEmpty()){
+   if(fromList.peekFirst() > toList.peekFirst()) {
+    return;
+   }
+  }
+  
+  int first = fromList.poll();
+  toList.addFirst(first);
+  
+ }
+ private LinkedList<Integer> getListFromChar(char lmr){
+  
+  switch (lmr) {
+  case 'l':
+   return l;
+  case 'm':
+   return m;
+  case 'r':
+   return r;
+  default:
+   return null;
+  }
+  
+ }
+ public void run() {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  while (true) {
+   try {
+    System.out.println(this);
+    System.out.print("Enter source and target stick (will move top piece):");
+    String s = br.readLine();
+    if (s.matches("^([lmr])([lmr])$")) {
+     char source = s.charAt(0);
+     char target = s.charAt(1);
+     movePiece(source, target);
+    }
+   } catch (Exception e) {
+    System.out.println("Try again, something's not right.");
+    // e.printStackTrace();
+   } 
+  }
+ }
+ private Iterator<Integer> getLeftDescendingIterator() {
+  return l.descendingIterator();
 
-	}
-	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
-	}
-	
-	public String toString() {
-		StringBuilder b = new StringBuilder();
-		b.append("  |\n l|");
-		Iterator<Integer> iter;
-		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n m|");
-		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |\n r|");
-		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
-			b.append(iter.next());
-			b.append(' ');
-		}
-		b.append("\n  |");
-		return b.toString();
-	}
-	
-	public static void main(String[] args) {
-		Hanoi hanoi = new Hanoi();
-		hanoi.run();
-	}
+ }
+ private Iterator<Integer> getMiddleDescendingIterator() {
+  return m.descendingIterator();
+
+ }
+ private Iterator<Integer> getRightDescendingIterator() {
+  return r.descendingIterator();
+ }
+ public String toString() {
+  StringBuilder b = new StringBuilder();
+  b.append(" |\n l|");
+  Iterator<Integer> iter;
+  iter = this.getLeftDescendingIterator();
+  while(iter.hasNext()) {
+   b.append(iter.next());
+   b.append(' ');
+  }
+  b.append("\n |\n m|");
+  iter = this.getMiddleDescendingIterator();
+  while(iter.hasNext()) {
+   b.append(iter.next());
+   b.append(' ');
+  }
+  b.append("\n |\n r|");
+  iter = this.getRightDescendingIterator();
+  while(iter.hasNext()) {
+   b.append(iter.next());
+   b.append(' ');
+  }
+  b.append("\n |");
+  return b.toString();
+ }
+ public static void main(String[] args) {
+  Hanoi hanoi = new Hanoi();
+  hanoi.run();
+ }
 
 }
